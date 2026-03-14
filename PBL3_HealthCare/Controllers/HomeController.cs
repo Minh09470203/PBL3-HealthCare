21413b0cfae4a98a1c58727d2f4b35bd8ad2d69f
pthinhhdev <phuthinhn2410@gmail.com>
2026-03-14 21:41:59 +0700

    update index

--- PBL3_HealthCare/Controllers/HomeController.cs
+++ PBL3_HealthCare/Controllers/HomeController.cs
@@ -1,21 +1,31 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PBL3_HealthCare.Models;
+using PBL3_HealthCare.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace PBL3_HealthCare.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Lấy Top 4 bác sĩ từ database
+            var topDoctors = _context.Doctors
+                                     .Take(4)
+                                     .ToList();
+
+            // truyền sang View
+            return View(topDoctors);
         }
 
         public IActionResult Privacy()
@@ -26,7 +36,10 @@ namespace PBL3_HealthCare.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
