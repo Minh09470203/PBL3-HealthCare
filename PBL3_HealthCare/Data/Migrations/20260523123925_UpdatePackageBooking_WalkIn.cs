95b977840f6a477376aa2c7983c00de530560c4a
Minh09470203 <10a10nguyenducminh@gmail.com>
2026-05-23 19:44:47 +0700

    Update UI labels to Tên đăng nhập and remove redundant texts

---
+++ PBL3_HealthCare/Data/Migrations/20260523123925_UpdatePackageBooking_WalkIn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PBL3_HealthCare.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePackageBooking_WalkIn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SelectedOptions",
+                table: "PackageBookings",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SelectedOptions",
+                table: "PackageBookings");
+        }
+    }
+}
